3a0aebefe7625635a72822276c3cc39d49f2a924
get-chance <50015863+get-chance@users.noreply.github.com>
2024-05-27 19:35:41 +0900

    설명 추가

--- flight_reservation_system/airlines_server.py
+++ flight_reservation_system/airlines_server.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 import pandas as pd
 
+# 인터페이스니 무시해도 됨.
 class server(metaclass = ABCMeta):
     @abstractmethod
     def check_flight_schedule_one_way(self, departure_date, departing_from, arriving_at):
@@ -18,28 +19,38 @@ class server(metaclass = ABCMeta):
     def cancel_flight(self, booking_reference):
         pass
     
-
-# 1. 지연 로딩/즉시 로딩 측면
-# 2. 중간에서 처리 측면
+# 클라이언트와 항공사 서버 중간에 위치하는 프록시 서버.
+# 클라이언트는 항공사 서버가 아닌 프록시 서버와 통신한다.
+# 프록시 서버를 둔 이유 : 성수기(peak_season)을 조회할 시 항공사 서버까지 갈 필요 없이 프록시 서버에서 조회
 class Proxy_server(server):
+
+    # 프록시 서버의 생성자
+    # 입력 : 항공사 서버
+    # 반환 : 반환값 없음
     def __init__(self, server):
         self.airlines_server = server
-        self.peak_season = None  # 여러개 일 경우 list()를 고려할 수 있음
+        self.peak_season = None
         self.peak_season_flight_schedule = None
 
+    # 성수기(peak_season) 기간 설정 함수
+    # 입력 : 성수기(peak_season)
+    # 반환 : 없음
+    # 주의 사항 : 프록시 서버로부터 항공편을 조회하기 전에 반드시 이 함수를 통해 peak_season을 정하여야 함
     def add_peak_season(self, start_date, end_date):
         df = self.airlines_server.flight_schedule
         
         self.peak_season = (start_date, end_date)
         self.peak_season_flight_schedule = df[(start_date <= df['Date']) & (df['Date'] <= end_date)]
 
-
+    # 편도편 조회 함수
+    # 입력 : "departure_date" = 출발일, "departing_from" = 출발지, "arriving_at" : 도착지
+    # 반환 : ([항공편 id, 항공편 id, ...], [{조회된 항공편 정보}, {조회된 항공편 정보}])
+    # 반환 값은 리스트 2개로 구성된 tuple이며, 첫번째 리스트는 항공편 id들을 가지고, 두번째 리스트는 조회된 항공편 정보를 dict형태로 가지고 있다.
+    # 반환 값으로 받은 조회된 항공편 정보를 보기 쉽게 dataframe으로 바꾸는 방법은 아래 테스트 코드를 참조하길 바란다.
     def check_flight_schedule_one_way(self, departure_date, departing_from, arriving_at):
         if (self.peak_season[0] <= departure_date and departure_date <= self.peak_season[1]):
             df = self.peak_season_flight_schedule
             df = df.loc[(df['Date'] == departure_date) & (df['Departure city'] == departing_from) & (df['Arrival city'] == arriving_at)]
-            
-            print(df)
 
             if (df.empty):
                 return None, None
@@ -50,6 +61,8 @@ class Proxy_server(server):
             print("loading from original server")
             return self.airlines_server.check_flight_schedule_one_way(departure_date, departing_from, arriving_at)
 
+    # 왕복편 조회 함수
+    # 아직 이용하지 말 것.
     def check_flight_schedule_round_trip(self, departure_date, return_date, departing_from, arriving_at):
         if (self.peak_season[0] <= departure_date and return_date <= self.peak_season[1]):
             _, out_bound_flights = self.check_flight_schedule_one_way(departure_date, departing_from, arriving_at)
@@ -60,14 +73,19 @@ class Proxy_server(server):
             print("loading from original server")
             self.airlines_server.check_flight_schedule_round_trip(departure_date, return_date, departing_from, arriving_at)
 
+    # 항공편 예약 함수
+    # 입력 : "out_bound_flight_id" = 가는 항공편의 id, "in_bound_flight_id" = 오는 항공편의 id, "passengers_info" = 승객 정보, "payment_method_or_agencyID" = 결제 수단'
+    # 반환 : ( 예약 성공 여부, 예약 번호(booking_reference) ) - tuple 형태로 반환
     def book_flight(self, out_bound_flight_id, in_bound_flight_id, passenger_count, passengers_info, payment_method_or_agencyID):
         return self.airlines_server.book_flight(out_bound_flight_id, in_bound_flight_id, passenger_count, passengers_info, payment_method_or_agencyID)
 
+    # 항공편 취소 함수
+    # 입력 : 예약 번호(booking_reference)
+    # 반환 : 예약 취소 여부
     def cancel_flight(self, booking_reference):
-        return self.airlines_server.cancle(booking_reference)
+        return self.airlines_server.cancel_flight(booking_reference)
 
-# server라는 클래스를 하나씩 두고, 각 개체를 만들까?
-# 아니면 각 항공사 서버별로 클래스를 만들고, singleton pattern을 적용할까?
+# 읽지 않아도 되는 클래스
 class AsianaAir_server:
     gen_reference_number = 0
 
@@ -95,8 +113,6 @@ class AsianaAir_server:
         df = self.flight_schedule
         df = df.loc[(df['Date']== departure_date) & (df['Departure city'] == departing_from) & (df['Arrival city'] == arriving_at)]
 
-        print(df)
-
         if (df.empty):
             return None, None
 
@@ -109,6 +125,7 @@ class AsianaAir_server:
         _, in_bound_flights = self.check_flight_schedule_one_way(return_date, arriving_at, departing_from)
 
         # 반환값 고민중..
+
     # 예약
     def book_flight(self, out_bound_flight_id, in_bound_flight_id, passenger_count, passengers_info, payment_method_or_agencyID):
         # schedule_id를 토대로 서버에서 최신의 데이터를 얻어옴
@@ -176,38 +193,45 @@ class JejuAir_server:
 DATA_DIR = ".\\data\\"
 asiana_schedule = DATA_DIR + "asiana_air_flight_schedule (24.7.1 ~ 24.7.10).xlsx"
 
+# 본 서버 생성
 asiana_air_server = AsianaAir_server()
 asiana_air_server.import_flight_schedule_from_xlsx(asiana_schedule)
-
-'''
-flight_id, info = asiana_air_server.check_flight_schedule_one_way("2024-07-04", "인천(ICN)", "로마(FCO)")
-
-asiana_air_server.check_flight_schedule_round_trip("2024-07-04", "2024-07-09", "인천(ICN)", "로마(FCO)")
-
-passengers_info = [("Raymond", "M", "1977-05-14"), ("Emma", "W", "1999-04-19"), ("Julia", "W", "2010-12-25")]
-b1, book_reference1 = asiana_air_server.book_flight(flight_id, None, 3, passengers_info, "whyparemore")
-
-flight_id, info = asiana_air_server.check_flight_schedule_one_way("2024-07-03", "인천(ICN)", "파리(CDG)")
-passengers_info = [("Billy", "M", "1974-08-17"), ("watson", "M", "1960-04-19")]
-b2, book_reference2 = asiana_air_server.book_flight(flight_id, None, 2, passengers_info, "goodtrip")
-
-b3 = asiana_air_server.cancel_flight(book_reference1)
-
-flight_id, info = asiana_air_server.check_flight_schedule_one_way("2024-07-04", "인천(ICN)", "로마(FCO)")
-passengers_info = [("Raymond", "M", "1977-05-14"), ("Emma", "W", "1999-04-19"), ("Julia", "W", "2010-12-25")]
-b1, book_reference1 = asiana_air_server.book_flight(flight_id, None, 3, passengers_info, "whyparemore")
-'''
-
-# 왕복편 테스트 필요.
-
 
 # 프록시 서버 테스트
 proxy_server = Proxy_server(asiana_air_server)
 proxy_server.add_peak_season("2024-07-02", "2024-07-05")
 
-print(proxy_server.check_flight_schedule_one_way("2024-07-04", "인천(ICN)", "로마(FCO)"))
-print(proxy_server.check_flight_schedule_one_way("2024-07-09", "로마(FCO)", "인천(ICN)"))
+# 편도편 조회
+flight_ids1, flight_infos1 = proxy_server.check_flight_schedule_one_way("2024-07-04", "인천(ICN)", "로마(FCO)")
+flight_ids2, flight_infos2 = proxy_server.check_flight_schedule_one_way("2024-07-09", "로마(FCO)", "인천(ICN)")
 
-proxy_server.check_flight_schedule_round_trip("2024-07-04", "2024-07-05", "인천(ICN)", "로마(FCO)")
+info_count = len(flight_ids1)
+for i in range(info_count):
+    print("flight id : " + str(flight_ids1[i]))
 
+    df = pd.DataFrame([flight_infos1[i]])
+    print(df)
+
+info_count = len(flight_ids2)
+for i in range(info_count):
+    print("flight id : " + str(flight_ids2[i]))
+
+    df = pd.DataFrame([flight_infos2[i]])
+    print(df)
+
+# 왕복편 조회 <- 사용 금지, 아직 구현하지 않음
+# proxy_server.check_flight_schedule_round_trip("2024-07-04", "2024-07-05", "인천(ICN)", "로마(FCO)") 
+
+# 예약
+flight_id = flight_ids1[0]
+passengers_info = [("Billy", "M", "1974-08-17"), ("watson", "M", "1960-04-19")]
+t_or_f, booking_reference = proxy_server.book_flight(flight_id, None, 2, passengers_info, "goodtrip")
+
+print("예약 성공 여부 : ", str(t_or_f))
+print("예약 번호 : ", str(booking_reference))
+
+# 예약 취소
+t_or_f = proxy_server.cancel_flight(booking_reference)
+
+print("예약 취소 여부 : ", str(t_or_f))
 
